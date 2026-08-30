cc4705dc5e1ffa19d72a1110cd27c967b9fdedae
91395 <913959180@qq.com>
2026-08-30 17:44:50 +0800

    improve LOPO update results

--- codings/tests/correlation_leave_one_participant_out_analysis_scores.py
+++ codings/tests/correlation_leave_one_participant_out_analysis_scores.py
@@ -39,6 +39,9 @@ def LOPO_correlation(var1, var2, df):
     print(f'original Pearson\'s r: {pearsonr(list1, list2)[0]} ,p: {pearsonr(list1, list2)[1]}')
     print(f'original Spearman\'s rho: {spearmanr(list1, list2)[0]} ,p: {spearmanr(list1, list2)[1]}')
 
+    original_pearson = pearsonr(list1, list2)[0]
+    original_spearman = spearmanr(list1, list2)[0]
+
     simu_pearson = []
     simu_spearman = []
 
